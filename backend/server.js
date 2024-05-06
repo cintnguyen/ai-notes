7bcf3578fc409e276cd9ce76b79e5029234f9d0c
cintnguyen <cintnguyen98@gmail.com>
2024-05-06 19:09:40 -0400

    feat: included del button

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import cors from 'cors'
-import './passport_conf.js' 
+import './passport_conf.js'
 import express from 'express'
 import passport from 'passport'
 import session from 'express-session'
@@ -18,10 +18,10 @@ mongoose.connect(process.env.MONGODB_URL, {
 }).then(() => {
   console.log('Connected to MongoDB');
 })
-.catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-  process.exit(1)
-});
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1)
+  });
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.geminiKey)
@@ -29,7 +29,7 @@ const genAI = new GoogleGenerativeAI(process.env.geminiKey)
 const app = express()
 const PORT = process.env.PORT
 
-app.use(cors({credentials: true, origin: true}))
+app.use(cors({ credentials: true, origin: true }))
 app.use(express.json())
 
 // express session 
@@ -77,13 +77,13 @@ app.get("/failed", (req, res) => {
 })
 
 // Success route if the authentication is successful
-app.get("/success",isLoggedIn, (req, res) => {
+app.get("/success", isLoggedIn, (req, res) => {
   console.log('You are logged in')
   res.send(`Welcome ${req.user.displayName}, your userID is ${req.user.id}`)
 })
 
 app.get("/userdata", (req, res) => {
-  res.json({username: req.user.displayName})
+  res.json({ username: req.user.displayName })
 })
 
 // Route that logs out the authenticated user  
@@ -118,13 +118,13 @@ app.post("/api", async (req, res) => {
   const text = req.body.text
   const aiResp = await talkToAi(text)
   try {
-    const conversation = new ConversationModel({question : text, answer : aiResp});
+    const conversation = new ConversationModel({ question: text, answer: aiResp });
     console.log(conversation)
 
     await conversation.save();
-    
+
     console.log(aiResp)
-    res.json({message: aiResp})
+    res.json({ message: aiResp })
 
   } catch (error) {
     console.log("api error", error)
@@ -137,11 +137,36 @@ app.get("/conversations", async (req, res) => {
     // Use the find() method on your ConversationModel to retrieve all conversations
     const conversations = await ConversationModel.find();
     res.json(conversations);
-    
+
   } catch (error) {
     console.error('Error retrieving conversations:', error);
     throw error; // You can handle or propagate the error as needed
   }
 })
+
+app.delete('/coversations/:id', async (req, res) => {
+  try {
+    const documentId = req.params.id;
+    console.log("HEYY", documentId)
+    
+    const filterQuery = { _id: new ObjectID(documentId) };
+    const collection = database.collection('Conversation'); // Replace 'your_collection_name' with the name of your collection
+
+    const result = await collection.deleteOne(filterQuery);
+
+    // const deleteResult = await deleteDocument('conversations', filterQuery);
+
+
+    if (result.deletedCount === 1) {
+      res.status(200).json({ message: 'Document deleted successfully.' });
+    } else {
+      res.status(404).json({ error: 'Document not found.' });
+    }
+  }
+  catch (error) {
+    console.error("Error deleting document:", error);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+});
 
 app.listen(PORT, () => console.log("server running on port: " + PORT))
